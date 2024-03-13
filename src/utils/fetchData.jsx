d8f8e7c56160aae277bbc306acd8c2e5806803d8
fgrka <katarzyna-fgr@gmail.com>
2024-03-13 22:38:41 +0100

    added process.env for vercel deployment

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const fetchData = async (url) => {
- 
+    const token = import.meta.env.VITE_RAPID_API ?? process.env.VITE_RAPID_API;
     const options = {
       method: 'GET',
       headers: {
-        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API,
+        'X-RapidAPI-Key': token,
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
       },
       params: {limit: '50'},
